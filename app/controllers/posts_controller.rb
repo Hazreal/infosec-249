cc13ff2a630a9e1012ed04d2607603941c98c4b6
Adam <cloakedzargon@gmail.com>
2016-08-29 20:59:11 -0500

    stuff needed for Posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  def index
+    @posts = Post.all.order("created_at desc") #.paginate(page: params[:page], per_page: 10)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+
+    if @post.save
+      redirect_to @post, notice: "Post Worked... I hope!"
+    else
+      render 'new', notice: "Didn't save the post"
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update post_params
+      redirect_to @post, notice: "Your article was saved!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :slug)
+    end
+end
+
+def find_post
+  @post = Post.find(params[:id])
+end
